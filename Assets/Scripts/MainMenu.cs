094e012724fc012a14ab52d0fe89afcc504bc28b
Technoo1 <1021945@student.sae.edu.au>
2023-02-24 14:39:37 +0800

    Added Audio Asset

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,7 +9,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Loads into the scene
-        
+
     }
 
     public void GoToMainMenu()
